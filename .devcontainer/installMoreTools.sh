44e8ca25bcc399744c6852bfc93add884ab4fd92
Mathieu Benoit <mathieu.benoit@humanitec.com>
2024-05-09 10:47:14 +0000

    No need to sudo on chmods
    Signed-off-by: Mathieu Benoit <mathieu.benoit@humanitec.com>

--- .devcontainer/installMoreTools.sh
+++ .devcontainer/installMoreTools.sh
@@ -6,19 +6,19 @@ cd install-more-tools
 SCORE_COMPOSE_VERSION=$(curl -sL https://api.github.com/repos/score-spec/score-compose/releases/latest | jq -r .tag_name)
 wget https://github.com/score-spec/score-compose/releases/download/${SCORE_COMPOSE_VERSION}/score-compose_${SCORE_COMPOSE_VERSION}_linux_amd64.tar.gz
 tar -xvf score-compose_${SCORE_COMPOSE_VERSION}_linux_amd64.tar.gz
-sudo chmod +x score-compose
+chmod +x score-compose
 sudo mv score-compose /usr/local/bin
 
 SCORE_HELM_VERSION=$(curl -sL https://api.github.com/repos/score-spec/score-helm/releases/latest | jq -r .tag_name)
 wget https://github.com/score-spec/score-helm/releases/download/${SCORE_HELM_VERSION}/score-helm_${SCORE_HELM_VERSION}_linux_amd64.tar.gz
 tar -xvf score-helm_${SCORE_HELM_VERSION}_linux_amd64.tar.gz
-sudo chmod +x score-helm
-sudo mv score-helm /usr/local/bin
+chmod +x score-helm
+mv score-helm /usr/local/bin
 
 KIND_VERSION=$(curl -sL https://api.github.com/repos/kubernetes-sigs/kind/releases/latest | jq -r .tag_name)
 curl -Lo ./kind https://kind.sigs.k8s.io/dl/${KIND_VERSION}/kind-linux-amd64
 chmod +x ./kind
-sudo mv ./kind /usr/local/bin/kind
+mv ./kind /usr/local/bin/kind
 
 cd ..
 rm -rf install-more-tools

88dcca5b74b28733e26a5b9b5792470d134af7cf
Mathieu Benoit <mathieu.benoit@humanitec.com>
2024-05-09 10:47:47 +0000

    But still need to sudo on mvs
    Signed-off-by: Mathieu Benoit <mathieu.benoit@humanitec.com>

--- .devcontainer/installMoreTools.sh
+++ .devcontainer/installMoreTools.sh
@@ -13,12 +13,12 @@ SCORE_HELM_VERSION=$(curl -sL https://api.github.com/repos/score-spec/score-helm
 wget https://github.com/score-spec/score-helm/releases/download/${SCORE_HELM_VERSION}/score-helm_${SCORE_HELM_VERSION}_linux_amd64.tar.gz
 tar -xvf score-helm_${SCORE_HELM_VERSION}_linux_amd64.tar.gz
 chmod +x score-helm
-mv score-helm /usr/local/bin
+sudo mv score-helm /usr/local/bin
 
 KIND_VERSION=$(curl -sL https://api.github.com/repos/kubernetes-sigs/kind/releases/latest | jq -r .tag_name)
 curl -Lo ./kind https://kind.sigs.k8s.io/dl/${KIND_VERSION}/kind-linux-amd64
 chmod +x ./kind
-mv ./kind /usr/local/bin/kind
+sudo mv ./kind /usr/local/bin/kind
 
 cd ..
 rm -rf install-more-tools
